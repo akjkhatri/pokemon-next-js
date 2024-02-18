4b94745374dd8d7657454481344d2b32d4ff0b84
akjkhatri <akjkhatri@gmail.com>
2024-02-18 23:21:43 +0000

    Step 4

--- src/components/pokemon-list/pokemonList.tsx
+++ src/components/pokemon-list/pokemonList.tsx
@@ -1,5 +1,13 @@
-import React from "react";
-import { Card, CardContent, Typography, Grid, Box } from "@mui/material";
+"use client";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Box,
+  TextField,
+} from "@mui/material";
 import PokemonStatesAndTabs from "../pokemon-states/pokemonStates";
 import PokemonTypes from "../pokemon-types/pokemonTypes";
 
@@ -8,36 +16,71 @@ interface PokemonListProp {
 }
 
 const PokemonList = ({ pokemons }: PokemonListProp) => {
+  const [searchText, setSearchText] = useState("");
+  const [filteredPokemons, setFilteredPokemons] = useState(pokemons);
+
+  const handleSearchChange = (event) => {
+    const searchText = event.target.value;
+    setSearchText(searchText);
+
+    const filtered = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredPokemons(filtered);
+  };
+
   return (
-    <Grid container spacing={2} justifyContent="center">
-      {pokemons.map((pokemon) => (
-        <Grid item key={pokemon.id} xs={12} sm={6} md={4}>
-          <Card style={{ background: "#424242", color: "white" }}>
-            <CardContent>
-              <Grid container spacing={2} alignItems="center">
-                <Grid item xs={12} md={6}>
-                  <Typography variant="h6" component="h2" gutterBottom>
-                    {pokemon.name.charAt(0).toUpperCase() +
-                      pokemon.name.slice(1)}
-                  </Typography>
-                  <img
-                    src={`/sprites/${pokemon.id}.svg`}
-                    alt={pokemon.name}
-                    style={{
-                      maxWidth: "100%",
-                      maxHeight: 200,
-                      marginBottom: "10px",
-                    }}
-                  />
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <TextField
+        label="Search Pokemon"
+        variant="outlined"
+        value={searchText}
+        onChange={handleSearchChange}
+        style={{
+          marginBottom: "15px",
+          maxWidth: 500,
+          color: "white",
+          background: "#424242",
+          width: "100%",
+        }}
+        InputLabelProps={{ style: { color: "white" } }}
+        InputProps={{
+          style: {
+            color: "white",
+          },
+          inputProps: { style: { color: "white" } },
+        }}
+      />
+      <Grid container spacing={2} justifyContent="center">
+        {filteredPokemons.map((pokemon) => (
+          <Grid item key={pokemon.id} xs={12} sm={6} md={4}>
+            <Card style={{ background: "#424242", color: "white" }}>
+              <CardContent>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12} md={6}>
+                    <Typography variant="h6" component="h2" gutterBottom>
+                      {pokemon.name.charAt(0).toUpperCase() +
+                        pokemon.name.slice(1)}
+                    </Typography>
+                    <img
+                      src={`/sprites/${pokemon.id}.svg`}
+                      alt={pokemon.name}
+                      style={{
+                        maxWidth: "100%",
+                        maxHeight: 200,
+                        marginBottom: "10px",
+                      }}
+                    />
+                  </Grid>
+                  <PokemonTypes types={pokemon.types} />
                 </Grid>
-                <PokemonTypes types={pokemon.types} />
-              </Grid>
-              <PokemonStatesAndTabs stats={pokemon.stats} />
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+                <PokemonStatesAndTabs stats={pokemon.stats} />
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 };
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ import { env } from "process";
 export const POKEMON_URL =
   env.POKEMON_URL || "https://pokeapi.co/api/v2/pokemon?limit=151";
 
-export const DEFAULT_DATA_PROVIDER = env.DEFAULT_DATA_PROVIDER || "LOCAL_DB";
+export const DEFAULT_DATA_PROVIDER =
+  env.DEFAULT_DATA_PROVIDER || "POKEMON_PARTNER";
 
 export const LOCAL_POKEMON_API =
   env.LOCAL_POKEMON_API || "http://localhost:3000/api/pokemon";
